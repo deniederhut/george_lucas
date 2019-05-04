bc1ec0a933f7dc7c7b8ddcd529796f676013a224
Dillon Niederhut <dillon.niederhut@gmail.com>
2019-05-04 08:42:26 -0500

    ENH: adds offline support
    Adds frames as json data file that can be played in the event
    that the telnet application fails.

--- george_lucas/projector.py
+++ george_lucas/projector.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 #! -*- encoding: utf-8 -*-
 
+
+import json
+import os
+from pkg_resources import resource_string
 import telnetlib
 import time
 
 
-class Reel(object):
+class TelnetReel(object):
 
     def __init__(self, host, frame_break):
         self.host = host
@@ -30,21 +34,57 @@ class Reel(object):
             self.source.close()
 
 
-class Movie(object):
+class JsonReel(object):
+
+    def __init__(self, movie_title, frame_break):
+        self.title = movie_title
+        self.frame_break = frame_break
+        self.load_reel(movie_title)
+
+    def load_reel(self, title):
+        data = resource_string(__name__, os.path.join('data', title + '.json'))
+        self.reel = json.loads(data.decode('utf-8'))
 
     def __iter__(self):
-        self.reel = Reel(self.host, self.frame_break)
         for frame in self.reel:
             yield frame
 
+
+class Movie(object):
+
+    def __init__(self):
+        raise NotImplementedError("Movie class is not meant to be used directly")
+
+    def __iter__(self):
+        for frame in self.reel:
+            yield frame
+
+
     def start_movie(self):
-        for frame in self:
-            print(frame)
-            time.sleep(self.frame_rate)
+        self.reel = TelnetReel(self.host, self.frame_break)
+        try:
+            for frame in self:
+                print(frame)
+                time.sleep(self.frame_rate)
+        except Exception as e:
+            print("\nTelnet application has failed with error :"
+                  "\n{!s} : {!s}.\nTrying local cache.".format(type(e), e))
+            time.sleep(10)
+            self.reel = JsonReel(self.movie_title, self.frame_break)
+            for frame in self:
+                print(frame)
+                time.sleep(self.frame_rate)
 
 
 class StarWars(Movie):
 
     host = 'Towel.blinkenlights.nl'
+    movie_title = 'a_new_hope'
     frame_break = b'\x1b[H'
-    frame_rate = 0.5
+
+    def __init__(self, frame_rate=0.5):
+        self.frame_rate = frame_rate
+
+
+if __name__ == '__main__':
+    pass
